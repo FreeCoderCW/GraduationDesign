93d2fceb217fd0cc258d692a3e41f8c5176e3794
程威 <33893974+FreeCoderCW@users.noreply.github.com>
2020-03-10 21:17:12 +0800

    人物移动与跳跃
    测试场景搭建
    人物动画导入
    人物移动功能：目前实现左右移动与跳跃（包括二段跳）以及下蹲（不包括动画）

---
+++ Graduation Design/Assets/MyGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private Animator anim;
+
+    [Header("移动参数")]
+    public float speed = 8f;
+    public float crouchSpeedDivisor = 3f;
+
+    [Header("跳跃参数")]
+    public float jumpForce = 6.3f;
+
+
+    float jumpTime;
+
+    [Header("状态")]
+    public bool isCrouch;
+    public bool isOnGround;
+    public bool isJump;
+
+    [Header("环境检测")]
+    public LayerMask groundLayer;
+
+    float xVelocity;
+
+    //按键设置
+    bool jumpPressed;
+    bool crouchHeld;
+
+    //碰撞体尺寸
+    Vector2 colliderStandSize;
+    Vector2 colliderStandOffset;
+    Vector2 colliderCrouchSize;
+    Vector2 colliderCrouchOffset;
+
+    public Transform groundCheck;
+    
+    int jumpCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+
+        colliderStandSize = coll.size;
+        colliderStandOffset = coll.offset;
+        colliderCrouchSize = new Vector2(coll.size.x, coll.size.y / 2f);
+        colliderCrouchOffset = new Vector2(coll.offset.x,coll.offset.y - coll.size.y/4f);     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount > 0)
+            jumpPressed = true;
+
+        crouchHeld = Input.GetButton("Crouch");
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+        PhysicsCheck();
+
+        GroundMovement();
+
+        Jump();
+
+        SwitchAnim();
+    }
+
+    void PhysicsCheck()
+    {
+        isOnGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+    }
+
+    void GroundMovement()
+    {
+        if (crouchHeld && !isCrouch && isOnGround)
+            Crouch();
+        else if (!crouchHeld && isCrouch)
+            StandUp();
+        else if (!isOnGround && isCrouch)
+            StandUp();
+
+        xVelocity = Input.GetAxis("Horizontal");
+
+        if (isCrouch)
+            xVelocity /= crouchSpeedDivisor;
+
+        rb.velocity = new Vector2(xVelocity * speed, rb.velocity.y);
+
+        FilpDirection();
+        
+    }
+
+    void Jump()
+    {
+        if (isOnGround)
+        {
+            jumpCount = 2;
+            isJump = false;
+        }
+        if (jumpPressed && isOnGround)
+        {
+            isJump = true;
+            //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        else if (jumpPressed && jumpCount > 0 && isJump)//二段跳
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            jumpCount--;
+            jumpPressed = false;
+        }
+    }
+
+    void FilpDirection()
+    {
+        if (xVelocity < 0)
+            transform.localScale = new Vector2(-1, 1);
+        if (xVelocity > 0)
+            transform.localScale = new Vector2(1, 1);
+
+    }
+
+    void Crouch()
+    {
+        isCrouch = true;
+        coll.size = colliderCrouchSize;
+        coll.offset = colliderCrouchOffset;
+    }
+
+    void StandUp()
+    {
+        isCrouch = false;
+        coll.size = colliderStandSize;
+        coll.offset = colliderStandOffset;
+    }
+
+    
+
+    
+
+    void SwitchAnim()
+    {
+        anim.SetFloat("running", Mathf.Abs(rb.velocity.x));
+
+        
+    }
+}
+
+
