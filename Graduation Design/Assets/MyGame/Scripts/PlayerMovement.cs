1982cd39aa841bda7ad4708c12a58602506d48f8
程威 <33893974+FreeCoderCW@users.noreply.github.com>
2020-03-11 20:40:50 +0800

    下蹲与悬挂功能实现
    解决了若干bug，学习了射线判断技巧。

--- Graduation Design/Assets/MyGame/Scripts/PlayerMovement.cs
+++ Graduation Design/Assets/MyGame/Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("跳跃参数")]
     public float jumpForce = 6.3f;
+    public float hangingJumpForce = 15f;
 
 
     float jumpTime;
@@ -22,8 +23,18 @@ public class PlayerMovement : MonoBehaviour
     public bool isCrouch;
     public bool isOnGround;
     public bool isJump;
+    public bool isHeadBlocked;
+    public bool isHanging;
 
     [Header("环境检测")]
+    public float footOffset = 0.47f;
+    public float headClearance = 0.5f;
+    public float groundDistance = 0.2f;
+    float playerHeight;
+    public float eyeHeight = 0.12f;
+    public float grabDistance = 0.4f;
+    public float reachOffset = 0.7f;
+
     public LayerMask groundLayer;
 
     float xVelocity;
@@ -31,12 +42,20 @@ public class PlayerMovement : MonoBehaviour
     //按键设置
     bool jumpPressed;
     bool crouchHeld;
+    bool crouchPressed;
 
     //碰撞体尺寸
     Vector2 colliderStandSize;
     Vector2 colliderStandOffset;
     Vector2 colliderCrouchSize;
     Vector2 colliderCrouchOffset;
+
+    //碰撞体中心距上下左右边界的距离
+    float up = 0.5f;
+    float down = 0.76f;
+    float left = 0.44f;
+    //float right = 0.5f;
+
 
     public Transform groundCheck;
     
@@ -49,6 +68,7 @@ public class PlayerMovement : MonoBehaviour
         coll = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
 
+        playerHeight = up;
         colliderStandSize = coll.size;
         colliderStandOffset = coll.offset;
         colliderCrouchSize = new Vector2(coll.size.x, coll.size.y / 2f);
@@ -62,6 +82,7 @@ public class PlayerMovement : MonoBehaviour
             jumpPressed = true;
 
         crouchHeld = Input.GetButton("Crouch");
+        crouchPressed = Input.GetButtonDown("Crouch");
 
     }
 
@@ -79,14 +100,53 @@ public class PlayerMovement : MonoBehaviour
 
     void PhysicsCheck()
     {
-        isOnGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+        //脚底射线
+        RaycastHit2D leftCheck = Raycast(new Vector2(-left, -down), Vector2.down, groundDistance, groundLayer);
+        RaycastHit2D rightCheck = Raycast(new Vector2(left, -down), Vector2.down, groundDistance, groundLayer);
+
+        if (leftCheck || rightCheck)
+            isOnGround = true;
+        else isOnGround = false;
+
+        //isOnGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
+
+        //头顶射线
+        RaycastHit2D leftheadCheck = Raycast(new Vector2(-left, coll.offset.y + coll.size.y /2f), Vector2.up, headClearance, groundLayer);
+        RaycastHit2D rightheadCheck = Raycast(new Vector2(left, coll.offset.y + coll.size.y / 2f), Vector2.up, headClearance, groundLayer);
+
+        if (leftheadCheck||rightheadCheck)
+            isHeadBlocked = true;
+        else isHeadBlocked = false;
+
+        float direction = transform.localScale.x;
+        Vector2 grabDir = new Vector2(direction, 0f);
+        RaycastHit2D blockedCheck = Raycast(new Vector2(footOffset * direction, playerHeight), grabDir, grabDistance, groundLayer);
+        RaycastHit2D wallCheck = Raycast(new Vector2(footOffset * direction, eyeHeight), grabDir, grabDistance, groundLayer);
+        RaycastHit2D ledgeCheck = Raycast(new Vector2(reachOffset * direction, playerHeight), Vector2.down, grabDistance, groundLayer);
+
+        if (!isOnGround && rb.velocity.y < 0f && ledgeCheck && wallCheck && !blockedCheck)
+        {
+            Vector3 pos = transform.position;
+
+            pos.x += (wallCheck.distance -0.05f) * direction;
+            pos.y -= ledgeCheck.distance;
+
+            transform.position = pos;
+
+            rb.bodyType = RigidbodyType2D.Static;
+            isHanging = true;
+        }
     }
 
     void GroundMovement()
     {
+        //判断悬挂
+        if (isHanging) return;
+
+
         if (crouchHeld && !isCrouch && isOnGround)
             Crouch();
-        else if (!crouchHeld && isCrouch)
+        else if (!crouchHeld && isCrouch && !isHeadBlocked)
             StandUp();
         else if (!isOnGround && isCrouch)
             StandUp();
@@ -104,12 +164,30 @@ public class PlayerMovement : MonoBehaviour
 
     void Jump()
     {
+
+        if (isHanging)
+        {
+            jumpCount=1;
+            if (jumpPressed)
+            {
+                rb.bodyType = RigidbodyType2D.Dynamic;
+                rb.velocity = new Vector2(rb.velocity.x, hangingJumpForce);
+                isHanging = false;
+            }
+        }
+        if (crouchPressed)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+
+            isHanging = false;
+        }
+
         if (isOnGround)
         {
             jumpCount = 2;
             isJump = false;
         }
-        if (jumpPressed && isOnGround)
+        if (jumpPressed && isOnGround && !isHeadBlocked)
         {
             isJump = true;
             //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
@@ -117,7 +195,7 @@ public class PlayerMovement : MonoBehaviour
             jumpCount--;
             jumpPressed = false;
         }
-        else if (jumpPressed && jumpCount > 0 && isJump)//二段跳
+        else if (jumpPressed && jumpCount > 0 && isJump && !isHeadBlocked)//二段跳
         {
             //rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
@@ -149,7 +227,18 @@ public class PlayerMovement : MonoBehaviour
         coll.offset = colliderStandOffset;
     }
 
-    
+    RaycastHit2D Raycast(Vector2 offset,Vector2 rayDirection,float length,LayerMask layer)
+    {
+        Vector2 pos = transform.position;
+
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, rayDirection, length, layer);
+
+        Color color = hit ? Color.red : Color.green;
+
+        Debug.DrawRay(pos + offset, rayDirection * length,color);
+
+        return hit;
+    }
 
     
 
